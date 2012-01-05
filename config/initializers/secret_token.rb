de3e7f8ba3dfeca969e074f9776e01a2eef7fa4c
ZVRS Pairing Station 2 <dev@zvrs.com>
2012-01-05 15:08:48 -0500

    Adding initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShowYourInterest::Application.config.secret_token = '2866a3a10d823b4356e34e5d9cceea1712e3aed38274b3a4978f078b759f8ce2d6be8f036b3c3f006dba2b75affd6b9bfb92b98f4653b38701e8c948f88076dc'
